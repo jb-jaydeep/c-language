07a052f35b6f9b11b81e504d4cdd2a53b39d9977
jb-jaydeep <jaydeepborda846@gmail.com>
2022-09-22 19:43:30 +0530

    file added

---
+++ 1JAYDEEP.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int x,y;
+    clrscr();
+    printf("ENTER A VALUE OF X = ");
+    scanf("%d",&x);
+    printf("ENTER A VALUE OF Y = ");
+    scanf("%d",&y);
+    printf("(x+y)^2 = %d",(x*x)+(2*x*y)+(y*y));
+    getch();
+}
