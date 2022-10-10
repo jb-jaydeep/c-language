448dd6ac5045a026b4a022dd00088c6085c48d20
jb-jaydeep <jaydeepborda846@gmail.com>
2022-10-11 01:17:27 +0530

    10-10-2022

---
+++ ch-8/ch-8-2/pattern with space/ch-8-2-16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+ char i,k,j;
+
+  for(i='A'; i<='E'; i++)
+ {
+  for(k='A'; k<i; k++)
+  {
+   printf(" ");
+  }
+  for(j=i; j<='E'; j++)
+  {
+   printf("%c",j);
+  }
+  printf("\n");
+ }
+
+}
