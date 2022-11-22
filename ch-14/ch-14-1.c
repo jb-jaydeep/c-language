2c0e331294d9bb82b9f3ca3e78e0eb214ff194e8
jb-jaydeep <114164037+jb-jaydeep@users.noreply.github.com>
2022-11-23 00:30:31 +0530

    Delete ch-14 directory

--- ch-14/ch-14-1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-	FILE *fp,*f1;
-	char name[100];
-	fp = fopen("C:\\Users\\jaydeepb borda\\Dropbox\\PC\\Desktop\\hello.txt","r");
-	f1 = fopen("C:\\Users\\jaydeepb borda\\Dropbox\\PC\\Desktop\\hello1.txt","a");
-	fgets(name,100,fp);
-	fprintf(f1,"%s\n",name);
-}
